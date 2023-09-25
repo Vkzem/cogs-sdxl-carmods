f95ad4f866ccb2ae031e64cfeed519a9a4bafd36
VK <vishalkhanna2906@gmail.com>
2023-09-25 15:39:28 +1000

    kill non used pipes, add s2k lora output

--- csript.sh
+++ csript.sh
@@ -2,23 +2,36 @@
 sudo curl -o /usr/local/bin/cog -L "https://github.com/replicate/cog/releases/latest/download/cog_$(uname -s)_$(uname -m)"
 sudo chmod +x /usr/local/bin/cog
 
-sudo apt-get install git
-# sudo apt-get install docker
-sudo apt update && \
-sudo apt install apt-transport-https ca-certificates curl software-properties-common -y && \
-curl -fsSL https://download.docker.com/linux/debian/gpg | sudo apt-key add - && \
-sudo add-apt-repository "deb [arch=amd64] https://download.docker.com/linux/debian $(lsb_release -cs) stable" && \
-sudo apt update && \
-sudo apt install docker-ce -y && \
-sudo systemctl enable docker && \
-sudo systemctl start docker && \
-sudo docker --version
-
+# sudo apt-get install git
+# # sudo apt-get install docker
+# sudo apt update && \
+# sudo apt install apt-transport-https ca-certificates curl software-properties-common -y && \
+# curl -fsSL https://download.docker.com/linux/debian/gpg | sudo apt-key add - && \
+# sudo add-apt-repository "deb [arch=amd64] https://download.docker.com/linux/debian $(lsb_release -cs) stable" && \
+# sudo apt update && \
+# sudo apt install docker-ce -y && \
+# sudo systemctl enable docker && \
+# sudo systemctl start docker && \
+# sudo docker --version
 
 # Get docker permissions
 sudo usermod -aG docker $USER
 newgrp docker
 sudo systemctl restart docker
 
+
 #clone this repo!
+
+sudo curl -o /usr/local/bin/cog -L "https://github.com/replicate/cog/releases/latest/download/cog_$(uname -s)_$(uname -m)"
+sudo chmod +x /usr/local/bin/cog
+
+mkdir working
+cd working
+
 git clone https://github.com/Vkzem/cog-sdxl-test.git    
+
+cd cog-sdxl-test
+
+cog predict -i prompt='a red car'
+
+cog train -i input_images=@data.zip
